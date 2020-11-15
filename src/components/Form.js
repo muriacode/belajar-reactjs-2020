cab53ec8bf6e75fefd7f752e0c711090c6f762de
Gilang Adi S <gilangas@outlook.com>
2020-11-15 21:01:06 +0700

    Create Reactjs form

---
+++ src/components/Form.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "./form.css"
+const Form = () => {
+    return(
+        <div>
+            <form>
+                <label for="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+
+                <label for="lname">Last Name</label>
+                <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+
+                <label for="country">Country</label>
+                <select id="country" name="country">
+                <option value="australia">Australia</option>
+                <option value="canada">Canada</option>
+                <option value="usa">USA</option>
+                </select>
+            
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    )
+}
+
+export default Form
